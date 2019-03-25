0c76ebc1b298a821f7c070e2ead8e7f89d70a48e
Rina Mito <satoshikibu@gmail.com>
2019-03-25 09:32:31 +0900

    4-7

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,10 @@
 class TasksController < ApplicationController
   def index
-    @tasks = Task.where(user_id: current_user.id)
+    @tasks = current_user.tasks.order(created_at: desc)
   end
 
   def show
-    @task = current_user.tasks.find(prams[:id])
+    @task = current_user.tasks.find(params[:id])
   end
 
   def new
@@ -12,17 +12,17 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = current_user.tasks.find(prams[:id])
+    @task = current_user.tasks.find(params[:id])
   end
 
   def update
-    task = current_user.tasks.find(prams[:id])
+    task = current_user.tasks.find(params[:id])
     task.update!(task_params)
     redirect_to tasks_url, notice: "タスク「#{task.name}」を更新しました。"
   end
 
   def destroy
-    task = current_user.tasks.find(prams[:id])
+    task = current_user.tasks.find(params[:id])
     task.destroy
     redirect_to tasks_url, notice: "タスク「#{task.name}を削除しました。」"
   end
@@ -31,7 +31,7 @@ class TasksController < ApplicationController
     @task = current_user.tasks.new(task_params)
 
     if @task.save
-      redirect_to @task, notice: "タスク「#{task.name}」を登録しました。"
+      redirect_to @task, notice: "タスク「#{@task.name}」を登録しました。"
     else
       render :new
     end
